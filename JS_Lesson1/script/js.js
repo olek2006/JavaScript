38b179e6b7613405f76e089d41fb0b962aaa47b4
olek2006 <oleksandryaremenko5@gmail.com>
2026-05-11 10:06:59 +0300

    class

---
+++ JS_Lesson1/script/js.js
@@ -0,0 +1,82 @@
+// console.log("Hello World!");
+// console.info("Hello World again!");
+// console.error("Hello World again1!");
+// console.warn("Hello World again2!");
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+
+// var MySecondVar = '23'
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MySecondVar - MyFirstVar);
+
+//
+// var int = 10;
+// var float = 12.5;
+// var boul = true; // false
+// var str = "6";
+
+// var a;
+// a = null;
+// console.log(a);
+
+// let a = 10,
+//     b = a + 2;
+
+// let a;
+// a = 10;
+// console.log(a);
+
+// let a = 10;
+// let a = 20;
+// console.log(a);
+
+//
+// const PI = 3.14;
+
+// let a = 10,
+//     b = 3;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// a = 2 + 2;
+// b = a + 10;
+
+// a = a + 2;
+// a *= 2;
+
+// a++; // суфіксний increment
+// a--; // суфіксний decrement
+
+// ++a; // префіксний increment
+// --a; // префіксний decrement
+
+// let a = 2,
+//     b;
+
+// b = ++a;
+
+// console.log(a);
+// console.log(b);
+
+// let a = '3',
+//     b = '5';
+
+// console.log((a - 0) + (b - 0));
+// console.log(Number(a) + Number(b));
+
+
+// -- ІНТЕРПОЛЯЦІЯ --
+
+let name = 'Ivan',
+    age = 18;
+
+console.log("Привіт " + name + ". Тобі " + age + " років");
+
+console.log(`Привіт ${name}, тобі ${age} років! ${2 + 2}`);
